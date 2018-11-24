0005b77daa364cfaef8fe5573832514f3d6ff4fa
Edwin <mredwinrodriguez@gmail.com>
2018-11-23 21:01:29 -0500

    network model

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # blue_directory
+Django Framework
+Djanto Templates
+MySQl
+
+Network platform for members of fraternities and sororities.
--- directory/networks/models.py
+++ directory/networks/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.utils.text import slugify
+import misaka
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 # Create your models here.
+ User = get_user_model()
+
+
+ class Network(models.Model):
+     name = models.Charfield(max-length=255, unique=True)
+     slug = models.Slugfield(allow_unicode=True, unique=True)
+     descrription = models.TextField(blank=True, default='')
+     description_html = models.textField(editable=False, default='', blank=True)
+     members = models.ManytoManyField(User, through='NetworkMember')
+
+     def __str__(self):
+         return self.name
+
+     def save(self, *args, **kwargs):
+         self.slug.slugify(self.name)
+         self.descrription_html = misaka.html(self.descrription)
+         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('networks:single', kwargs={'slug':self.slug})
+
+    class Meta:
+        ordering = ['name']
+
+
+
+
+ class NetworkMember(models.Model):
+     network = models.ForeignKey(Group, related_name="memberships")
+     user = models.ForeignKey(User, related_names="user_networks")
+
+
+     def __str__(self):
+         return self.user.username
+
+     class Meta:
+         unique_together = ('netowk, user')
+
+
+
+
